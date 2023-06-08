ec6977fe9fbcf053b2c7b085582b254338871ebc
jeevan kishore <jeevankishorer@gmail.com>
2023-06-08 20:17:32 +0530

    add file

---
+++ hello world 1/src/TernaryOperatorEG.java
@@ -0,0 +1,18 @@
+
+public class TernaryOperatorEG {
+
+	public static void main(String[] args) {
+
+		int time = 20;
+	    if (time < 18) {
+	      System.out.println("Good day.");
+	    } else {
+	      System.out.println("Good evening.");
+	    }  
+	    // Ternary Method
+	    String result;
+	    result = (time < 18) ? "Good day." : "Good evening.";
+	    System.out.println(result);
+	}
+
+}
