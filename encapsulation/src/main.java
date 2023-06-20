dea09d5bca4ae7844a301e238433f3f51680f6d3
jeevan kishore <jeevankishorer@gmail.com>
2023-06-20 12:53:56 +0530

    add file

---
+++ encapsulation/src/main.java
@@ -0,0 +1,9 @@
+
+public class main {
+
+	 public static void main(String[] args) {
+		    Person myObj = new Person();
+		    myObj.setName("John");
+		    System.out.println(myObj.getName());
+		  }
+}
---
+++ import/src/username/Person.java
@@ -0,0 +1,20 @@
+package username;
+import username.Person;
+
+ class Person {
+
+	 public static void main(String[] args) {
+		    Person myObj = new Person();
+		    String userName;
+		    
+		    System.out.println("Enter username"); 
+		    userName = myObj.nextLine();   
+		       
+		    System.out.println("Username is: " + userName);        
+		  }
+
+	private String nextLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
